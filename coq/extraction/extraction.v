6ccc4e4e44e7813a0e0a337c97c7e78991d7da8b
Qinshi Wang <qinshiw@cs.princeton.edu>
2020-10-19 21:51:23 +0800

    add extraction to Ocaml to compare with Petr4

---
+++ coq/extraction/extraction.v
@@ -0,0 +1,10 @@
+Require Import Types.
+Require Import Typed.
+Require Import Prog.
+Require Import ExtrOcamlBasic.
+Require Import ExtrOcamlString.
+
+Cd "extraction".
+
+Separate Extraction
+   Prog.Program.
