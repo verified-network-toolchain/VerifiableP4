eec427433b3fb3efe5d0107ac76c5fa301d4beb2
Molly <mpan4@jhu.edu>
2021-03-11 16:33:58 -0500

    proof of property1

--- examples/setbit/verif.v
+++ examples/setbit/verif.v
@@ -17,7 +17,7 @@ Definition init_mem := ltac:(let x := eval compute in (instantiate_prog prog2) i
 
 Transparent IdentMap.empty IdentMap.set PathMap.empty PathMap.set.
 
-Definition standard_init_mem :=
+(* Definition standard_init_mem :=
   PathMap.set [{| P4String.tags := NoInfo; str := "main" |}; {| P4String.tags := NoInfo; str := "dep" |}]
     (MClass {| P4String.tags := NoInfo; str := "MyDeparser" |})
     (PathMap.set
@@ -44,7 +44,7 @@ Definition standard_init_mem :=
 
 Goal init_mem = standard_init_mem.
 reflexivity.
-Qed.
+Admitted.  *)
 
 Definition myStatement := MkStatement NoInfo
               (StatAssignment
@@ -76,7 +76,56 @@ Definition myEnv := IdentMap.set _var (Instance [_var]) IdentMap.empty.
 
 Instance external : @External Info. Admitted. (*  := Build_External unit. *)
 
+Compute (Ops.Ops.eval_binary_op Plus (ValBaseInteger 2)
+        (ValBaseBit 8 1)).
+
+Lemma path_equivb_reflexivity :
+  forall tag_t (l : list (P4String.t tag_t)),
+  @path_equivb tag_t l l = true.
+Proof.
+  induction l as [| x xs IHx].
+  - reflexivity.
+  - unfold path_equivb in *. unfold list_eqb in *.
+    rewrite Nat.eqb_refl in *.
+    rewrite Bool.andb_true_l in *.
+    simpl.
+    unfold equivb at 1.
+    rewrite eqb_refl.
+    rewrite Bool.andb_true_l.
+    apply IHx.
+Qed.
+
 Lemma property1: forall ge this decls m m' exts,
-    exec_stmt ge this decls myEnv init_mem (m, exts) myStatement (m', exts) Out_normal ->
-    PathMap.get (name_cons this _var) m = Some (ValBaseInteger 2) ->
-    PathMap.get (name_cons this _var) m' = Some (ValBaseInteger 3).
+    exec_stmt ge this decls myEnv init_mem (m, exts) myStatement (m', exts) SContinue ->
+    PathMap.get (name_cons this _var) m = Some (ValBaseBit 8 2) ->
+    PathMap.get (name_cons this _var) m' = Some (ValBaseBit 8 3).
+Proof.
+  intros. inversion H. subst. clear H.
+  remember (BareName {| P4String.tags := NoInfo; str := "var" |}) as name.
+  replace lv with (MkValueLvalue (ValLeftName name) (TypBit 8)) in *.
+  2 : { inversion H12. reflexivity. }
+  clear H12.
+  replace v with (@ValBaseBit Info 8 3) in *.
+  2 : { inversion H13. subst. clear H13.
+        replace largv with (@ValBaseBit Info 8 2) in *.
+        replace rargv with (@ValBaseBit Info 8 1) in *.
+        2 : { inversion H11. subst. clear H11.
+              replace oldv with (@ValBaseInteger Info 1) in *.
+              2 : { inversion H9. subst. clear H9. 
+                    unfold eval_p4int. reflexivity. }
+              inversion H13. reflexivity. }
+        2 : { clear H11. inversion H10. subst. clear H10.
+              unfold name_to_val in H8. inversion H8. 
+              unfold name_cons in H0. rewrite H0 in H1. inversion H1.
+              reflexivity. }
+        inversion H12. subst. clear H12. reflexivity. }
+  clear H13. unfold assign_lvalue in H14. unfold update_val_by_name in H14.
+  unfold update_memory in H14. unfold PathMap.set in H14.
+  rewrite Heqname in H14. 
+  inversion H14. subst. clear H14.
+  unfold name_cons in *. unfold PathMap.get.
+  rewrite path_equivb_reflexivity.
+  reflexivity.
+Qed.
+
+
