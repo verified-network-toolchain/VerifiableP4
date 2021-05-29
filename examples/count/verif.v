be4a9ba885d304a108699ea8e97e0daadd1cc633
Qinshi Wang <qinshiw@cs.princeton.edu>
2021-05-29 14:18:16 +0800

    add verif in count

---
+++ examples/count/verif.v
@@ -0,0 +1,39 @@
+Require Import Poulet4.P4defs.
+Require Import Poulet4.P4Notations.
+Open Scope string_scope.
+
+Import ListNotations.
+Require Import ProD3.examples.count.p4ast.
+
+Require Import Poulet4.Maps.
+Require Import Poulet4.Semantics.
+Require Import Poulet4.SimplExpr.
+Require Import Poulet4.V1Model.
+Require Import ProD3.core.Hoare.
+Require Import ProD3.core.AssertionLang.
+
+Instance target : @Target Info (@Expression Info) := V1Model.
+
+Opaque IdentMap.empty IdentMap.set PathMap.empty PathMap.set.
+
+(* Global environment *)
+Definition ge := Eval compute in load_prog prog.
+
+(* Global environment for types *)
+Definition ge_typ := Eval compute in match gen_ge_typ prog with Some ge_typ => ge_typ | None => IdentMap.empty end.
+
+Axiom ge_senum : @genv_senum Info.
+
+Definition instantiation := Eval compute in instantiate_prog prog.
+
+(* inst_m *)
+Definition inst_m := Eval compute in fst instantiation.
+
+(* Initial extern state *)
+Definition init_es := Eval compute in snd instantiation.
+
+Notation ident := (P4String.t Info).
+Notation path := (list ident).
+
+Transparent IdentMap.empty IdentMap.set PathMap.empty PathMap.set.
+
