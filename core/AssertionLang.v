4f37d9cbe757e447514f13670877f982e356dc66
Qinshi Wang <qinshiw@cs.princeton.edu>
2021-05-31 21:15:03 +0800

    prove that update_by_loc in the assertion language simulates update_by_loc in Semantics

--- core/AssertionLang.v
+++ core/AssertionLang.v
@@ -4,6 +4,8 @@ Require Import Poulet4.Typed.
 Require Import Poulet4.Syntax.
 Require Import Poulet4.Semantics.
 Require Import ProD3.core.Coqlib.
+Require Import Hammer.Tactics.Tactics.
+Require Import Hammer.Plugin.Hammer.
 
 Section AssertionLang.
 
@@ -27,46 +29,23 @@ Definition satisfies_unit (st : state) (a_unit : @Locator tags_t * Val) : Prop :
   loc_to_val this loc st = Some v.
 
 Definition satisfies (st : state) (a : assertion) : Prop :=
-  fold_left and (map (satisfies_unit st) a) True.
+  fold_right and True (map (satisfies_unit st) a).
 
 Definition to_shallow_assertion (a : assertion) : state -> Prop :=
   fun st => satisfies st a.
 
-Definition no_overlapping_unit_unit (a_unit1 a_unit2 : @Locator tags_t * Val) : bool :=
-  let (loc1, _) := a_unit1 in
+Definition no_overlapping_unit (loc1 : @Locator tags_t) (a_unit2 : @Locator tags_t * Val) : bool :=
+  (* let (loc1, _) := a_unit1 in *)
   let (loc2, _) := a_unit2 in
   match loc1, loc2 with
   | LInstance p1, LInstance p2 => ~~ (path_equivb p1 p2)
   | _, _ => false
   end.
 
-Fixpoint no_overlapping_unit (a_unit : @Locator tags_t * Val) (a : assertion) : bool :=
+Fixpoint no_overlapping (loc : @Locator tags_t) (a : assertion) : bool :=
   match a with
-  | hd :: tl => no_overlapping_unit_unit a_unit hd && no_overlapping_unit a_unit tl
+  | hd :: tl => no_overlapping_unit loc hd && no_overlapping loc tl
   | [] => true
-  end.
-
-Fixpoint no_overlapping (a : assertion) : bool :=
-  match a with
-  | hd :: tl => no_overlapping_unit hd tl && no_overlapping tl
-  | [] => true
-  end.
-
-Definition locator_equiv (loc1 loc2 : @Locator tags_t) : bool :=
-  match loc1, loc2 with
-  | LInstance p1, LInstance p2 => path_equivb p1 p2
-  | LGlobal p1, LGlobal p2 => path_equivb p1 p2
-  | _, _ => false
-  end.
-
-Fixpoint update_val_by_loc (a : assertion) (loc : Locator) (v : Val) : assertion :=
-  match a with
-  | (hd_loc, hd_v) :: tl =>
-      if locator_equiv loc hd_loc then
-        (hd_loc, v) :: tl
-      else
-        (hd_loc, hd_v) :: update_val_by_loc tl loc v
-  | [] => [(loc, v)]
   end.
 
 Definition is_instance (loc : @Locator tags_t) : bool :=
@@ -75,33 +54,115 @@ Definition is_instance (loc : @Locator tags_t) : bool :=
   | LGlobal _ => false
   end.
 
-(* Lemma update_val_by_loc_sound : forall st a loc v,
-  no_overlapping a ->
-  is_instance loc = true ->
+Fixpoint wellformed (a : assertion) : bool :=
+  match a with
+  | hd :: tl =>
+      let (loc, _) := hd in
+      is_instance loc && no_overlapping loc tl && wellformed tl
+  | [] => true
+  end.
+
+Definition locator_equivb (loc1 loc2 : @Locator tags_t) : bool :=
+  match loc1, loc2 with
+  | LInstance p1, LInstance p2 => path_equivb p1 p2
+  | LGlobal p1, LGlobal p2 => path_equivb p1 p2
+  | _, _ => false
+  end.
+
+Fixpoint update_val_by_loc (a : assertion) (loc : Locator) (v : Val) : assertion :=
+  match a with
+  | hd :: tl =>
+      let (hd_loc, hd_v) := hd in
+      if locator_equivb loc hd_loc then
+        (hd_loc, v) :: tl
+      else
+        hd :: update_val_by_loc tl loc v
+  | [] => [(loc, v)]
+  end.
+
+Axiom loc_to_val_update_val_by_loc_same : forall st p1 p2 v,
+  path_equivb p1 p2 ->
+  loc_to_val this (LInstance p1) (Semantics.update_val_by_loc this st (LInstance p2) v) = Some v.
+
+Axiom loc_to_val_update_val_by_loc_different : forall st p1 p2 v,
+  ~~ path_equivb p1 p2 ->
+  loc_to_val this (LInstance p1) (Semantics.update_val_by_loc this st (LInstance p2) v) =
+    loc_to_val this (LInstance p1) st.
+
+Axiom path_equivb_refl : forall (p : path),
+  path_equivb p p.
+
+Axiom path_equivb_symm : forall (p1 p2 : path),
+  path_equivb p1 p2 = path_equivb p2 p1.
+
+Axiom path_equivb_trans : forall (p1 p2 p3 : path),
+  path_equivb p1 p2 -> path_equivb p2 p3 -> path_equivb p1 p3.
+
+Lemma update_val_by_loc_different st a loc v :
+  no_overlapping loc a ->
+  to_shallow_assertion a st ->
+  to_shallow_assertion a (Semantics.update_val_by_loc this st loc v).
+Proof.
+  intros H_no_overlapping H_pre.
+  induction a; intros.
+  - sfirstorder.
+  - split.
+    + destruct a as [a_loc a_v]. simpl in *. destruct loc; destruct a_loc; only 1-3 : sfirstorder.
+      rewrite loc_to_val_update_val_by_loc_different. { hauto use: path_equivb_symm unfold: is_true, andb, negb. }
+      sfirstorder.
+    + apply IHa; [hauto b: on | sfirstorder].
+Qed.
+
+Lemma update_val_by_loc_different' st a loc v loc' :
+  locator_equivb loc loc' ->
+  no_overlapping loc' a ->
+  to_shallow_assertion a st ->
+  to_shallow_assertion a (Semantics.update_val_by_loc this st loc v).
+Proof.
+  intros H_equiv H_no_overlapping H_pre.
+  induction a; intros.
+  - sfirstorder.
+  - split.
+    + destruct a as [a_loc a_v]. simpl in *. destruct loc; destruct loc'; destruct a_loc; only 1-7 : sfirstorder.
+      rewrite loc_to_val_update_val_by_loc_different.
+        { simpl in H_equiv. hfcrush brefl: on use: path_equivb_symm, Reflect.negE, path_equivb_trans unfold: andb, is_true, negb. }
+      sfirstorder.
+    + apply IHa; [hauto b: on | sfirstorder].
+Qed.
+
+Lemma update_val_by_loc_add st a loc v loc' :
+  is_instance loc ->
+  no_overlapping loc' a ->
+  locator_equivb loc loc' ->
+  to_shallow_assertion a st ->
+  to_shallow_assertion ((loc', v) :: a) (Semantics.update_val_by_loc this st loc v).
+Proof.
+  intros H_no_overlapping H_pre.
+  split; only 2 : (eapply update_val_by_loc_different'; hauto).
+  destruct loc; destruct loc'; only 1-3 : sfirstorder.
+  apply loc_to_val_update_val_by_loc_same; unfold locator_equivb in H0; srun eauto use: path_equivb_symm.
+Qed.
+
+Lemma update_val_by_loc_sound : forall st a loc v,
+  wellformed a ->
+  is_instance loc ->
   to_shallow_assertion a st ->
   to_shallow_assertion (update_val_by_loc a loc v) (Semantics.update_val_by_loc this st loc v).
 Proof.
-  intros * H_no_overlapping H_is_instance H_pre.
+  intros * H_wellformed H_is_instance H_pre.
   destruct loc; only 1 : inversion H_is_instance.
   unfold to_shallow_assertion in *.
   induction a; intros.
-  - destruct st. simpl. unfold satisfies. simpl. repeat split.
-    unfold loc_to_val. simpl.
-    replace (PathMap.get (this ++ p) (PathMap.set (this ++ p) v m)) with (Some v) by admit.
-    reflexivity.
-  - assert (satisfies (Semantics.update_val_by_loc this st (LInstance p) v)
-        (update_val_by_loc a0 (LInstance p) v)). {
-      apply IHa.
-      - simpl in H_no_overlapping. Search reflect and. move /andP : H_no_overlapping => H_no_overlapping. apply H_no_overlapping. *)
+  - unfold satisfies. repeat split.
+    apply loc_to_val_update_val_by_loc_same. srun eauto use: path_equivb_refl.
+  - destruct a as [hd_loc hd_v]. simpl. destruct hd_loc; only 1 : hauto lq: on.
+    destruct (path_equivb p p0) eqn:H_path_equivb.
+    + apply update_val_by_loc_add; only 2 : hauto b: on; sfirstorder.
+    + split.
+      * simpl. rewrite loc_to_val_update_val_by_loc_different. { hauto use: path_equivb_symm unfold: negb, is_true. }
+        sfirstorder.
+      * apply IHa; [hauto b: on | sfirstorder].
+Qed.
 
 End AssertionLang.
-
-
-
-
-
-
-
-
-
 
