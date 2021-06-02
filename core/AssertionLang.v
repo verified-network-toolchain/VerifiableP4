b09456b8c2fb877aa2df9632a17b35880d5b8b2d
Qinshi Wang <qinshiw@cs.princeton.edu>
2021-06-02 23:10:40 +0800

    eval expressions with assertion

--- core/AssertionLang.v
+++ core/AssertionLang.v
@@ -1,5 +1,4 @@
 Require Import Coq.ssr.ssrbool.
-Require Import Coq.ssr.ssreflect.
 Require Import Poulet4.Typed.
 Require Import Poulet4.Syntax.
 Require Import Poulet4.Semantics.
@@ -108,7 +107,7 @@ Proof.
   - sfirstorder.
   - split.
     + destruct a as [a_loc a_v]. simpl in *. destruct loc; destruct a_loc; only 1-3 : sfirstorder.
-      rewrite loc_to_val_update_val_by_loc_different. { hauto use: path_equivb_symm unfold: is_true, andb, negb. }
+      rewrite loc_to_val_update_val_by_loc_different by (hauto use: path_equivb_symm unfold: negb, is_true, andb).
       sfirstorder.
     + apply IHa; [hauto b: on | sfirstorder].
 Qed.
@@ -124,8 +123,8 @@ Proof.
   - sfirstorder.
   - split.
     + destruct a as [a_loc a_v]. simpl in *. destruct loc; destruct loc'; destruct a_loc; only 1-7 : sfirstorder.
-      rewrite loc_to_val_update_val_by_loc_different.
-        { simpl in H_equiv. hfcrush brefl: on use: path_equivb_symm, Reflect.negE, path_equivb_trans unfold: andb, is_true, negb. }
+      rewrite loc_to_val_update_val_by_loc_different by (
+        simpl in H_equiv; hfcrush brefl: on use: path_equivb_symm, Reflect.negE, path_equivb_trans unfold: andb, is_true, negb).
       sfirstorder.
     + apply IHa; [hauto b: on | sfirstorder].
 Qed.
@@ -159,9 +158,78 @@ Proof.
     destruct (path_equivb p p0) eqn:H_path_equivb.
     + apply update_val_by_loc_add; only 2 : hauto b: on; sfirstorder.
     + split.
-      * simpl. rewrite loc_to_val_update_val_by_loc_different. { hauto use: path_equivb_symm unfold: negb, is_true. }
+      * simpl. rewrite loc_to_val_update_val_by_loc_different by (hauto use: path_equivb_symm unfold: negb, is_true).
         sfirstorder.
       * apply IHa; [hauto b: on | sfirstorder].
+Qed.
+
+Fixpoint get_val (a : assertion) (loc : Locator) : option Val :=
+  match a with
+  | (hd_loc, hd_v) :: tl =>
+      if locator_equivb hd_loc loc then Some hd_v else get_val tl loc
+  | [] => None
+  end.
+
+(* This axiom is provable if tags_t is a unit type. *)
+Axiom locator_equivb_eq : forall loc1 loc2, locator_equivb loc1 loc2 -> loc1 = loc2.
+
+Lemma get_val_sound : forall st a loc v,
+  wellformed a ->
+  to_shallow_assertion a st ->
+  get_val a loc = Some v ->
+  loc_to_val this loc st = Some v.
+Proof.
+  intros * H_wellformed H_pre H_get_val.
+  induction a as [ |hd tl].
+  - inversion H_get_val.
+  - destruct hd as [hd_loc hd_v].
+    simpl in H_wellformed, H_get_val, H_pre.
+    destruct (locator_equivb hd_loc loc) eqn:H_locator_equivb.
+    + hnf in H_pre. erewrite <- (locator_equivb_eq _ loc) by (apply H_locator_equivb).
+      sfirstorder.
+    + apply IHtl; only 1: hauto b: on; sfirstorder.
+Qed.
+
+Definition eval_expr (a : assertion) (expr : Expression) :=
+  eval_expr_gen (fun _ loc => get_val a loc) expr.
+
+Definition eval_expr_gen_refine_statement get_val1 get_val2 (expr : @Expression tags_t) v :=
+  (forall name loc v, get_val1 name loc = Some v -> get_val2 name loc = Some v) ->
+  eval_expr_gen get_val1 expr = Some v ->
+  eval_expr_gen get_val2 expr = Some v.
+
+Lemma eval_expr_gen_refine get_val1 get_val2 (expr : @Expression tags_t) v :
+  eval_expr_gen_refine_statement get_val1 get_val2 expr v
+with eval_expr_gen_refine_preT get_val1 get_val2 tags expr typ dir v :
+  eval_expr_gen_refine_statement get_val1 get_val2 (MkExpression tags expr typ dir) v.
+Proof.
+  - intros. destruct expr; apply eval_expr_gen_refine_preT.
+  - unfold eval_expr_gen_refine_statement. intros H_refine H_eval_expr_gen.
+    destruct expr; inversion H_eval_expr_gen.
+    + reflexivity.
+    + hauto lq: on.
+    + destruct (eval_expr_gen get_val1 arg) eqn:H_eval_arg; only 2 : inversion H1.
+      assert (eval_expr_gen get_val2 arg = Some v0) by (eapply eval_expr_gen_refine; eauto).
+      hauto lq: on.
+    + destruct args as [larg rarg].
+      destruct (eval_expr_gen get_val1 larg) eqn:H_eval_larg; only 2 : inversion H1.
+      destruct (eval_expr_gen get_val1 rarg) eqn:H_eval_rarg; only 2 : inversion H1.
+      assert (eval_expr_gen get_val2 larg = Some v0) by (eapply eval_expr_gen_refine; eauto).
+      assert (eval_expr_gen get_val2 rarg = Some v1) by (eapply eval_expr_gen_refine; eauto).
+      hauto lq: on.
+Qed.
+
+Lemma eval_expr_sound : forall ge_typ ge_senum st a expr v,
+  wellformed a ->
+  to_shallow_assertion a st ->
+  eval_expr a expr = Some v ->
+  exec_expr ge_typ ge_senum this st expr v.
+Proof.
+  intros * H_wellformed H_pre H_get_val.
+  apply eval_expr_gen_sound.
+  eapply eval_expr_gen_refine; only 2 : eassumption.
+  intros *. simpl.
+  apply get_val_sound; assumption.
 Qed.
 
 End AssertionLang.
