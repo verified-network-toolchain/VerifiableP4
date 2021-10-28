4f22ca6dc82768846caaebbeeccda0ffc27c576f
Molly Pan <pmy112@hotmail.com>
2021-10-28 02:17:25 -0400

    extern write spec done w/ issues

--- core/V1ModelSpec.v
+++ core/V1ModelSpec.v
@@ -7,6 +7,7 @@ Require Import ProD3.core.Coqlib.
 Require Import ProD3.core.Hoare.
 Require Import Poulet4.V1Model.
 Require Import Poulet4.P4Notations.
+Require Import Poulet4.P4Arith.
 Require Import BinNat.
 (* Require Import Hammer.Tactics.Tactics.
 Require Import Hammer.Plugin.Hammer. *)
@@ -38,12 +39,12 @@ Variable ge : (@genv tags_t).
 
 Definition register_read_pre (p : path) reg i :=
   fun (args : list Sval) st =>
-    args = [ValBaseBit (P4Arith.to_loptbool 32%N i)]
+    args = [ValBaseBit (to_loptbool 32%N i)]
     /\ PathMap.get p (get_external_state st) = Some (ObjRegister reg).
 
 Definition register_read_post (p : path) reg i :=
   fun (args : list Sval) st =>
-    args = [ValBaseBit (P4Arith.to_loptbool (reg_width reg) (Znth i (reg_content reg)))]
+    args = [ValBaseBit (to_loptbool (reg_width reg) (Znth i (reg_content reg)))]
     /\ PathMap.get p (get_external_state st) = Some (ObjRegister reg).
 
 Definition hoare_func_modifies (ge : genv) (p : path) (func : @fundef tags_t) (vars : list Locator) (exts : list Locator) :=
@@ -67,6 +68,31 @@ Definition register_read_spec : Prop :=
         (fun args _ => register_read_post p reg i args)
         [] [].
 
+Definition register_write_pre (p : path) reg i v :=
+  fun (args : list Sval) st =>
+    args = [ValBaseBit (to_loptbool 32%N i); ValBaseBit (to_loptbool (reg_width reg) v)]
+    /\ PathMap.get p (get_external_state st) = Some (ObjRegister reg).
 
+Definition register_write_post (p : path) reg i v :=
+  fun (args : list Sval) st =>
+    args = []
+    /\ v = Znth i (reg_content reg)
+    /\ PathMap.get p (get_external_state st) = Some (ObjRegister reg).
 
+Definition register_write_spec : Prop :=
+  forall (p : path) (reg : register) (i : Z) (v : Z),
+    hoare_func_spec ge p
+        (register_write_pre p reg i v)
+        (FExternal !"Register" !"write")
+        nil
+        (fun args _ => register_write_post p reg i v args)
+        [] [ LInstance [] ].
+
+(*
+Several issues:
+- out of bounds for i
+- to_lbool/to_loptbool is N-to-1 mapping, diffcult to automate
+=> also a problem in semantics.v
+- are v and i assumed to be bounded in post_condition?
+*)
 
