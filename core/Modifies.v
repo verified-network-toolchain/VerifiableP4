1b33098c4b8d1ee3b5928a5843b28a6f582b3a79
Qinshi Wang <qinshiw@cs.princeton.edu>
2022-01-23 00:58:09 -0500

    prove call_modifies_func

--- core/Modifies.v
+++ core/Modifies.v
@@ -85,54 +85,6 @@ Definition func_modifies (p : path) (func : @fundef tags_t) (vars : option (list
     exec_func ge read_ndetbit p st func targs inargs st' outargs sig ->
     modifies vars exts st st'.
 
-(* Ltac specialize_hoare_block :=
-  lazymatch goal with
-  | H : hoare_block _ _ _ _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption))
-  | H : forall _ _ _, _ -> exec_block _ _ _ _ _ _ _ -> _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption))
-  end.
-
-Ltac specialize_hoare_stmt :=
-  lazymatch goal with
-  | H : hoare_stmt _ _ _ _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption))
-  | H : forall _ _ _, _ -> exec_stmt _ _ _ _ _ _ _ -> _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption))
-  end.
-
-Ltac specialize_hoare_expr_det :=
-  lazymatch goal with
-  | H : hoare_expr_det _ _ _ _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption) ltac:(first [eassumption | repeat constructor]))
-  | H : forall _ _ _, _ -> exec_expr_det _ _ _ _ _ _ -> _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption) ltac:(first [eassumption | repeat constructor]))
-  end.
-
-Ltac specialize_hoare_lexpr :=
-  lazymatch goal with
-  | H : hoare_lexpr _ _ _ _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption))
-  | H : forall _ _ _, _ -> exec_lexpr _ _ _ _ _ _ _ -> _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption))
-  end.
-
-Ltac specialize_hoare_write :=
-  lazymatch goal with
-  | H : hoare_write _ _ _ _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption) ltac:(eassumption))
-  | H : forall _ _ _, _ -> exec_write _ _ _ _ _ -> _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption) ltac:(eassumption))
-  end.
-
-Ltac specialize_hoare_call :=
-  lazymatch goal with
-  | H : hoare_call _ _ _ _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption))
-  | H : forall _ _ _, _ -> exec_call _ _ _ _ _ _ _ -> _ |- _ =>
-      specialize (H _ _ _ ltac:(eassumption) ltac:(eassumption))
-  end. *)
-
 Lemma block_modifies_nil : forall p tags vars exts,
   block_modifies p (BlockEmpty tags) vars exts.
 Proof.
@@ -222,10 +174,10 @@ Qed.
 
 Local Hint Resolve write_modifies_intro : core.
 
-Lemma stmt_modifies_assign : forall p tags lhs rhs typ p' vars exts,
+Lemma stmt_modifies_assign : forall p tags lhs rhs typ lv vars exts,
   is_call_expression rhs = false ->
-  get_lexpr_base lhs = Some p' ->
-  In_vars p' vars ->
+  get_lexpr_base lhs = Some lv ->
+  In_vars lv vars ->
   stmt_modifies p (MkStatement tags (StatAssignment lhs rhs) typ) vars exts.
 Proof.
   unfold stmt_modifies. intros.
@@ -234,6 +186,98 @@ Proof.
     inv H11; inv H.
   }
   destruct sig; simpl in *; subst; eauto.
+Qed.
+
+Lemma stmt_modifies_assign_call : forall p tags lhs rhs typ lv vars exts,
+  is_call_expression rhs = true ->
+  call_modifies p rhs vars exts ->
+  get_lexpr_base lhs = Some lv ->
+  In_vars lv vars ->
+  stmt_modifies p (MkStatement tags (StatAssignment lhs rhs) typ) vars exts.
+Proof.
+  unfold stmt_modifies. intros.
+  inv H3. 1 : {
+    (* rule out the non-call case *)
+    pose proof (exec_expr_det_not_call _ _ _ _ _ _ H10).
+    congruence.
+  }
+  destruct sig0; only 2, 3, 4 : destruct H14; subst; eauto.
+  destruct sig'; only 1, 3, 4 : destruct H14; subst; eauto.
+  destruct H14 as [? []]; subst; eauto.
+Qed.
+
+Lemma call_modifies_builtin : forall p tags tags' expr fname tparams params typ' dir' args typ dir lv vars exts,
+  let dirs := map get_param_dir params in
+  get_lexpr_base expr = Some lv ->
+  In_vars lv vars ->
+  call_modifies p
+    (MkExpression tags (ExpFunctionCall
+      (MkExpression tags' (ExpExpressionMember expr fname) (TypFunction (MkFunctionType tparams params FunBuiltin typ')) dir)
+      nil args) typ dir')
+    vars exts.
+Proof.
+  unfold call_modifies.
+  intros.
+  inv H1. 2 : { inv H8. }
+  destruct sig0; only 2, 3, 4 : destruct H19; subst; eauto.
+  destruct sig'; only 1, 3, 4 : destruct H19; subst; eauto.
+  destruct H19; subst; eauto.
+Qed.
+
+Inductive out_arg_In_vars (vars : option (list path)) : option Expression -> direction -> Prop :=
+| out_arg_In_vars_in : forall expr,
+    out_arg_In_vars vars expr Typed.In
+| out_arg_In_vars_out_dontcare :
+    out_arg_In_vars vars None Out
+| out_arg_In_vars_out : forall expr lv,
+    get_lexpr_base expr = Some lv ->
+    In_vars lv vars ->
+    out_arg_In_vars vars (Some expr) Out
+| out_arg_In_vars_inout : forall expr lv,
+    get_lexpr_base expr = Some lv ->
+    In_vars lv vars ->
+    out_arg_In_vars vars (Some expr) InOut.
+
+Lemma call_modifies_func_part1 : forall p st0 st args dirs argvals sig outvals st' vars exts,
+  Forall2 (out_arg_In_vars vars) args dirs ->
+  exec_args ge read_ndetbit p st0 args dirs argvals sig ->
+  exec_call_copy_out ge (combine (map snd argvals) dirs) outvals st st' ->
+  modifies vars exts st st'.
+Proof.
+  intros * H.
+  revert st0 st argvals sig outvals st'.
+  induction H as [ | expr dir]; intros.
+  - inv H; inv H0; auto.
+  - inv H1. inv H9.
+    + eauto.
+    + inv H2. inv H5. eauto.
+    + inv H2. inv H6. inv H. eauto.
+    + inv H2. inv H9. inv H. eauto.
+Qed.
+
+Lemma call_modifies_func : forall p tags func targs args typ dir obj_path fd vars exts,
+  is_builtin_func func = false ->
+  let dirs := get_arg_directions func in
+  Forall2 (out_arg_In_vars vars) args dirs ->
+  lookup_func ge p func = Some (obj_path, fd) ->
+  func_modifies (force p obj_path) fd (if is_some obj_path then None else vars) exts ->
+  call_modifies p (MkExpression tags (ExpFunctionCall func targs args) typ dir) vars exts.
+Proof.
+  unfold call_modifies, func_modifies.
+  intros.
+  inv H3. { inv H. }
+  rewrite H1 in H14; inv H14.
+  assert (modifies vars exts st (if is_some obj_path0 then set_memory (get_memory st) s3 else s3)). {
+    destruct (is_some obj_path0).
+    - destruct st. destruct s3.
+      apply H2 in H18.
+      destruct H18; destruct vars; split; eauto.
+    - eauto.
+  }
+  assert (modifies vars exts (if is_some obj_path0 then set_memory (get_memory st) s3 else s3) s5). {
+    eapply call_modifies_func_part1; eauto.
+  }
+  destruct sig0; destruct H21; subst; eauto.
 Qed.
 
 End Modifies.
