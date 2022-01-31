d99de621b1f0f74f570318bc192ec5929175f642
Lennart <eberinge@cs.princeton.edu>
2022-01-31 09:36:08 -0500

    Finished vrification of first 2 test cases

--- examples/bloomfilter/main_theorem.v
+++ examples/bloomfilter/main_theorem.v
@@ -105,81 +105,42 @@ Proof.
     try solve [inv H0]; auto.
 Qed.
 
-(* Definition packet1: BinNums.Z * port := (Z0, port_int).
+Definition packet1: BinNums.Z * port := (Z0, port_int).
 Definition packet2: BinNums.Z * port := (1%Z, port_ext).
 
 Lemma Test1: forall st' output,
-     @process_packets _ _ ge custom_metadata_t  init_es  [packet1;packet2] st' output -> True.
+     @process_packets _ _ ge custom_metadata_t  init_es  [packet1;packet2] st' output ->
+     bf_encode st'
+        (add0 Z Z.eqb CRC_pad0 (empty Z) 0,
+         add1 Z Z.eqb CRC_pad1 (empty Z) 0,
+         add2 Z Z.eqb CRC_pad2 (empty Z) 0) /\
+    output = [Some (0, port_ext); None].
 Proof.
-intros. inv H. 
-unfold packet1 in H3.
-unfold packet2 in H6.
-inv H3. 
-specialize ( bloomfilter_body). intros.
-unfold FuncSpec.fundef_satisfies_spec in H.  destruct H. simpl in H.
-specialize (H 0%Z Z0 (empty Z, empty Z, empty Z)).
-unfold Hoare.hoare_func in H.
-inv H1. inv H2.
-apply H in H10. clear H.
-2:{ clear H H10. 
-  split. red. red. Tactics.Forall2_sval_refine.
-  split. red. red. simpl; trivial.
-  repeat constructor. }
-trivial.
+intros. inv H.
+assert (Bf: bf_encode init_es (empty Z, empty Z, empty Z)).
+{ split. reflexivity. split; reflexivity. } 
+eapply process_packet_prop in H3; [ clear Bf; destruct H3 as [bf1 [Proc1 Bf1]] | eassumption].
+inv Proc1. inv H6.
+eapply process_packet_prop in H2; [ clear Bf1; destruct H2 as [bf2 [Proc2 Bf2]] | eassumption].
+inv Proc2. inv H5. split; trivial.
 Qed.
+
 Definition packet1': BinNums.Z * port := (Z0, port_int).
 Definition packet2': BinNums.Z * port := (Z0, port_ext).
 
 Lemma Test2: forall es' output,
      @process_packets _ _ ge custom_metadata_t  init_es  [packet1';packet2'] es' output -> 
-     (*st' = add *)True.
+     bf_encode es'
+        (add0 Z Z.eqb CRC_pad0 (empty Z) 0,
+         add1 Z Z.eqb CRC_pad1 (empty Z) 0,
+         add2 Z Z.eqb CRC_pad2 (empty Z) 0) /\
+     output = [Some (0, port_ext); Some (0, port_int)].
 Proof.
-intros. inv H. Print  V1Model.extern_state.
-unfold packet1 in H3.
-unfold packet2 in H6.
-inv H3. 
-specialize ( bloomfilter_body). intros.
-unfold FuncSpec.fundef_satisfies_spec in H.  destruct H. simpl in H.
-specialize (H 0%Z Z0 (empty Z, empty Z, empty Z)).
-unfold Hoare.hoare_func in H.
-inv H1. inv H2.
-apply H in H10. clear H.
-2:{ clear H H10. 
-  split. red. red. Tactics.Forall2_sval_refine.
-  split. red. red. simpl; trivial.
-  repeat constructor. }
-inv H10. destruct H1 as [K1 [K2 K3]]. clear K1 K2.
-specialize ( bloomfilter_body). intros.
-unfold FuncSpec.fundef_satisfies_spec in H.  destruct H. simpl in H.
-specialize (H 0%Z Z0 (empty Z, empty Z, empty Z)).
-unfold Hoare.hoare_func in H.
-inv H1. inv H2.
-apply H in H10. clear H.
-2:{ clear H H10. 
-  split. red. red. Tactics.Forall2_sval_refine.
-  split. red. red. simpl; trivial.
-  repeat constructor. }
-inv H10. destruct H1 as [K1 [K2 K3]]. clear K1 K2.
-trivial.
+intros. inv H.
+assert (Bf: bf_encode init_es (empty Z, empty Z, empty Z)).
+{ split. reflexivity. split; reflexivity. } 
+eapply process_packet_prop in H3; [ clear Bf; destruct H3 as [bf1 [Proc1 Bf1]] | eassumption].
+inv Proc1. inv H6.
+eapply process_packet_prop in H2; [ clear Bf1; destruct H2 as [bf2 [Proc2 Bf2]] | eassumption].
+inv Proc2. inv H5. split; trivial.
 Qed.
-extern_state -> list (option (Z * port))
- trivial. red. red. simpl. split. reflexivity. split. reflexivity. 
-    subst hdr. 
-     simpl.  constructor.
-    + 
-  econstructor. simpl.  split. AssertionNotations.ARG.
-assert (AP: ["main"; "ig"] = inst_path). 
-{ clear H H10. inv H1; trivial. }
-subst inst_path.
-inv H2.
-apply H in H10.
-specialize (H  bloomfilter_state
-apply H in H10. simpl in H.  in H10.
-Check (FuncSpec.fundef_satisfies_spec _ _ tgt ge bloomfilter_spec).
-bloomfilter_spec.
-
-Check bloomfilter_body. Search FuncSpec.fundef_satisfies_spec Semantics.exec_func .
-Print FuncSpec.fundef_satisfies_spec_aux.
-Print FuncSpec.fundef_satisfies_hoare.
-Search Semantics.exec_func.
-inversion H3. simpl in *. *)
