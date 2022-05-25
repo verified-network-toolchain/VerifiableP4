a40379b514676dd97b1ef40c3d6133202bdbbfc9
Qinshi Wang <qinshiw@cs.princeton.edu>
2022-05-24 23:03:15 -0400

    prove execute_body for insert (admit some general lemmas)

--- examples/sbf/verif.v
+++ examples/sbf/verif.v
@@ -77,12 +77,15 @@ Definition Row_insert_spec : func_spec :=
         (MEM [(["rw"], eval_val_to_sval (ValBaseBit (P4Arith.to_lbool 8%N 1)))]
         (EXT [row_repr p (row_insert r i)]))).
 
+Definition num_cells := 65536.
+
 Definition Row_regact_insert_execute_spec : func_spec :=
   WITH (* p *),
     PATH p ++ ["regact_insert"]
     MOD None [p ++ ["reg_row"]]
     WITH (r : row) (i : Z)
-      (_ : 0 <= i < Zlength r),
+      (H_r : Zlength r = num_cells)
+      (H_i : 0 <= i < num_cells),
       PRE
         (ARG [eval_val_to_sval (ValBaseBit (P4Arith.to_lbool 16%N i))]
         (MEM []
@@ -166,7 +169,6 @@ Lemma Row_regact_insert_execute_body :
   fundef_satisfies_spec ge execute_fundef nil Row_regact_insert_execute_spec.
 Proof.
   split.
-  intro.
   2 : {
     unfold func_modifies; intros.
     inv H.
@@ -205,6 +207,143 @@ Proof.
       solve_modifies.
     }
     eapply modifies_set_ext with (st := (m, s')).
+    auto.
+  }
+  repeat lazymatch goal with
+  | |- fundef_satisfies_hoare _ _ _ _ (fsh_bind (fun x => _)) =>
+    let x := fresh x in intro x
+  end;
+  unfold fundef_satisfies_hoare.
+  unfold hoare_func; intros.
+  inv H0. inv H6.
+  cbn - [ge] in H0.
+  inv H0.
+  compute in H1. inv H1. (* get reg from ge *)
+  compute in H4. inv H4. (* get (w, size) from ge *)
+  destruct H as [? [_ ?]].
+  assert (index = i). {
+    clear -H_i H H3 H6.
+    unfold arg_denote, arg_satisfies in H.
+    remember (eval_val_to_sval (ValBaseBit (P4Arith.to_lbool 16 i))) as x in H.
+    inv H. inv H5.
+    inv H3. clear H7.
+    assert (ValBaseBit indexb = (ValBaseBit (P4Arith.to_lbool 16 i))). {
+      eapply exec_val_eq.
+      eapply exec_val_sym with eq.
+      { clear; auto. }
+      assert (val_to_sval
+          (ValBaseBit (P4Arith.to_lbool 16 i))
+          (eval_val_to_sval (ValBaseBit (P4Arith.to_lbool 16 i)))). {
+        eapply exec_val_sym with strict_read_ndetbit.
+        2 : eapply sval_to_val_eval_val_to_sval.
+        { clear; sauto. }
+        { clear; sauto. }
+      }
+      eapply exec_val_trans with (f := read_detbit);
+        [ | eapply exec_val_trans; [ | eassumption | eassumption] | eassumption ].
+      { unfold rel_trans; clear; sauto. }
+      { unfold rel_trans; clear; sauto. }
+    }
+    inv H.
+    rewrite P4Arith.bit_from_to_bool in H6.
+    inv H6.
+    apply Z.mod_small; assumption.
+  }
+  clear H H3 H6.
+  subst.
+  destruct (-1 <? i) eqn:Heqb; only 2 : lia. clear Heqb.
+  destruct (i <? 65536) eqn:Heqb; only 2 : (unfold num_cells in H_i; lia). clear Heqb.
+  compute -[target am_ge] in H2. inv H2.
+  change
+    (exec_abstract_method (tags_t := Info) (target := target) am_ge (p ++ ["regact_insert"])
+      Row_regact_insert_apply
+      es [old_value] s' [new_value; retv] SReturnNull)
+    in H8.
+  simpl in H7. destruct H9. subst.
+  assert (content = map bool_to_val r). {
+    clear -H0 H5.
+    inv H0.
+    simpl in H.
+    congruence.
+  }
+  clear H0.
+  subst.
+  assert (content' = map bool_to_val r). {
+    clear -H H5 H8.
+    inv H8.
+    apply (Row_regact_insert_apply_body) in H1.
+    destruct H1.
+    assert (PathMap.get ["pipe"; "ingress"; "bf2_ds"; "win_1"; "row_1"; "reg_row"] s'
+        = PathMap.get ["pipe"; "ingress"; "bf2_ds"; "win_1"; "row_1"; "reg_row"] es). {
+      symmetry.
+      apply H3.
+      auto.
+    }
+    rewrite H4 in H.
+    change (@extern_object Info (@Expression Info) (@extern_sem Info (@Expression Info) target))
+      with (@Tofino.object Info (@Expression Info)) in H.
+    rewrite H5 in H.
+    congruence.
+  }
+  clear H H5.
+  subst.
+  rewrite Znth_map in H8 by list_solve.
+  assert (new_value = ValBaseBit (P4Arith.to_lbool 8 1)
+      /\ retv = ValBaseBit (P4Arith.to_lbool 8 1)). {
+    clear -H8.
+    inv H8.
+    apply (proj1 Row_regact_insert_apply_body) in H0.
+    2 : {
+      split.
+      2 : { split; constructor. }
+      inv H. inv H4. inv H6.
+      do 9 lazymatch goal with
+      | H : Forall2 read_detbit _ _ |- _ =>
+          inv H
+      end.
+      do 8 lazymatch goal with
+      | H : read_detbit _ _ |- _ =>
+          inv H
+      end.
+      repeat constructor.
+    }
+    destruct H0.
+    inv H0. inv H7. inv H8.
+    assert (forall width value sv,
+      sval_refine (ValBaseBit (P4Arith.to_loptbool width value)) sv ->
+      sv = (ValBaseBit (P4Arith.to_loptbool width value))
+    ) as sval_refine_bit_to_loptbool by admit.
+    apply sval_refine_bit_to_loptbool in H4, H5; subst.
+    inv H1. inv H7. clear H8.
+    assert (forall width value v,
+      sval_to_val read_ndetbit (ValBaseBit (P4Arith.to_loptbool width value)) v ->
+      v = (ValBaseBit (P4Arith.to_lbool width value))
+    ) as sval_to_val_bit_to_loptbool by admit.
+    apply sval_to_val_bit_to_loptbool in H4, H5; subst.
+    auto.
+  }
+  clear H8.
+  destruct H; subst.
+  simpl.
+  split.
+  { inv H12. constructor. }
+  clear H12.
+  split.
+  { unfold ret_denote, ret_satisfies.
+    intros.
+    assert (forall width value sv,
+      val_to_sval (ValBaseBit (P4Arith.to_lbool width value)) sv ->
+      sv = (ValBaseBit (P4Arith.to_loptbool width value))
+    ) as val_to_sval_bit_to_lbool by admit.
+    apply val_to_sval_bit_to_lbool in H. subst.
+    repeat constructor.
+  }
+  split.
+  { constructor. }
+  { constructor.
+    2 : constructor.
+    simpl.
+    rewrite PathMap.get_set_same.
     auto.
   }
 Admitted.
