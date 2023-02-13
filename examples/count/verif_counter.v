2091b130ea35f3c663767061c95e2b3249c4aa56
Shengyi Wang <txyyss@gmail.com>
2023-02-13 15:26:10 -0500

    Update verif_counter.v

--- examples/count/verif_counter.v
+++ examples/count/verif_counter.v
@@ -4,7 +4,6 @@ Require Import ProD3.core.Core.
 Require Import Poulet4.P4light.Architecture.Tofino.
 Require Import ProD3.core.Tofino.
 Require Import ProD3.examples.count.common.
-
 Require Import ProD3.examples.count.ModelRepr.
 Require Import Hammer.Plugin.Hammer.
 Require Export Coq.Program.Program.
@@ -22,7 +21,6 @@ Open Scope func_spec.
 Definition regact_counter_apply_body :=
   ltac:(auto_regact ge am_ge (p ++ ["regact_counter"])).
 
-(* I would like to make it opaque, but I don't know how to. *)
 Definition regact_counter_execute_body :=
   ltac:(build_execute_body ge regact_counter_apply_body).
 
@@ -30,3 +28,27 @@ Definition regact_counter_execute_body :=
 
 Definition counter_act_fundef :=
   ltac:(get_fd ["SwitchIngress"; "act_counter"] ge).
+
+Definition counter_act_spec : func_spec :=
+  WITH (* p *),
+    PATH p
+    MOD (Some [["ig_md"]]) [p]
+    WITH (counter : Z),
+      PRE
+        (ARG []
+        (MEM []
+        (EXT [counter_repr p counter])))
+      POST
+        (ARG_RET [] ValBaseNull
+        (MEM [(["ig_md"; "num_pkt"], P4Bit 32 (counter + 1))]
+           (EXT [counter_repr p (counter + 1)]))).
+
+Lemma counter_act_body:
+  func_sound ge counter_act_fundef nil counter_act_spec.
+Proof.
+  start_function.
+  unfold counter_repr, counter_reg_repr.
+  normalize_EXT.
+  Intros_prop. simpl.
+  (* step_call regact_counter_execute_body. *)
+Abort.
